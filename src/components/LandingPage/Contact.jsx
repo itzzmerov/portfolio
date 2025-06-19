9ed5bf856942a8aada38fd86e64a77d498e49042
John Rovie Balingbing <balingbing.johnrovie20@gmail.com>
2025-06-19 16:09:40 +0800

    Applied responsiveness to Contact and Footer

--- src/components/LandingPage/Contact.jsx
+++ src/components/LandingPage/Contact.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { FaFacebook, FaLinkedin, FaFacebookMessenger } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import TriangleBlob from '../../assets/images/triangle-blob.png'
-import ManyTriangleBlob from '../../assets/images/many-triangle-blob.png'
+import TriangleBlob from '../../assets/images/triangle-blob.png';
+import ManyTriangleBlob from '../../assets/images/many-triangle-blob.png';
 
 const Contact = () => {
     const [name, setName] = useState('');
@@ -10,16 +10,30 @@ const Contact = () => {
     const [message, setMessage] = useState('');
 
     return (
-        <section id="contact" className="relative overflow-x-hidden px-6 md:px-28 py-20">
-            <h2 className="relative z-20 text-center text-custom-darkish-blue text-7xl font-titillium font-black underline mb-4">Contact Me</h2>
-            <div className="flex gap-10 justify-center mb-16">
-                <a href='mailto:balingbing.johnrovie20@gmail.com' className="text-custom-darkish-blue hover:text-custom-dark-pink"><MdEmail fontSize={'50px'} /></a>
-                <a href='https://www.messenger.com/t/itzzmerov' target='_blank' rel='noreferrer' className="text-custom-darkish-blue hover:text-custom-dark-pink"><FaFacebookMessenger fontSize={'50px'} /></a>
-                <a href='https://www.facebook.com/itzzmerov' target='_blank' rel='noreferrer' className="text-custom-darkish-blue hover:text-custom-dark-pink"><FaFacebook fontSize={'50px'} /></a>
-                <a href='https://www.linkedin.com/' target='_blank' rel='noreferrer' className="text-custom-darkish-blue hover:text-custom-dark-pink"><FaLinkedin fontSize={'50px'} /></a>
+        <section
+            id="contact"
+            className="relative overflow-x-hidden px-6 tablet:px-10 laptop:px-24 desktop:px-52 desktop-4k:px-80 py-20"
+        >
+            <h2 className="relative z-20 text-center text-custom-darkish-blue text-5xl tablet:text-6xl laptop:text-7xl font-titillium font-black underline mb-3 laptop:mb-8">
+                Contact Me
+            </h2>
+
+            <div className="flex flex-wrap gap-6 justify-center mb-16">
+                <a href="mailto:balingbing.johnrovie20@gmail.com" className="text-custom-darkish-blue hover:text-custom-dark-pink">
+                    <MdEmail fontSize={'40px'} />
+                </a>
+                <a href="https://www.messenger.com/t/itzzmerov" target="_blank" rel="noreferrer" className="text-custom-darkish-blue hover:text-custom-dark-pink">
+                    <FaFacebookMessenger fontSize={'40px'} />
+                </a>
+                <a href="https://www.facebook.com/itzzmerov" target="_blank" rel="noreferrer" className="text-custom-darkish-blue hover:text-custom-dark-pink">
+                    <FaFacebook fontSize={'40px'} />
+                </a>
+                <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer" className="text-custom-darkish-blue hover:text-custom-dark-pink">
+                    <FaLinkedin fontSize={'40px'} />
+                </a>
             </div>
-            <form className="max-w-2xl mx-auto flex flex-col gap-6 justify-center items-center px-4 md:px-0">
-                {/* Name */}
+
+            <form className="w-full max-w-xl mx-auto flex flex-col gap-6 justify-center items-center px-2 tablet:px-0">
                 <div className="relative w-full">
                     <input
                         type="text"
@@ -31,14 +45,13 @@ const Contact = () => {
                     />
                     <label
                         htmlFor="name"
-                        className={`absolute left-3 px-1 font-montserrat bg-[#EFFAFD] transition-all duration-200 text-custom-pink ${name ? 'top-[-10px] text-sm' : 'top-3.5 text-base'
-                            } peer-focus:top-[-10px] peer-focus:text-sm`}
+                        className={`absolute left-3 px-1 font-montserrat bg-[#EFFAFD] transition-all duration-200 text-custom-pink ${
+                            name ? 'top-[-10px] text-sm' : 'top-3.5 text-base'
+                        } peer-focus:top-[-10px] peer-focus:text-sm`}
                     >
                         Name
                     </label>
                 </div>
-
-                {/* Email */}
                 <div className="relative w-full">
                     <input
                         type="email"
@@ -50,14 +63,13 @@ const Contact = () => {
                     />
                     <label
                         htmlFor="email"
-                        className={`absolute left-3 px-1 font-montserrat bg-[#EFFAFD] transition-all duration-200 text-custom-pink ${email ? 'top-[-10px] text-sm' : 'top-3.5 text-base'
-                            } peer-focus:top-[-10px] peer-focus:text-sm`}
+                        className={`absolute left-3 px-1 font-montserrat bg-[#EFFAFD] transition-all duration-200 text-custom-pink ${
+                            email ? 'top-[-10px] text-sm' : 'top-3.5 text-base'
+                        } peer-focus:top-[-10px] peer-focus:text-sm`}
                     >
                         Email Address
                     </label>
                 </div>
-
-                {/* Message */}
                 <div className="relative w-full">
                     <textarea
                         id="message"
@@ -69,14 +81,17 @@ const Contact = () => {
                     ></textarea>
                     <label
                         htmlFor="message"
-                        className={`absolute left-3 px-1 font-montserrat bg-[#EFFAFD] transition-all duration-200 text-custom-pink ${message ? 'top-[-10px] text-sm' : 'top-3.5 text-base'
-                            } peer-focus:top-[-10px] peer-focus:text-sm`}
+                        className={`absolute left-3 px-1 font-montserrat bg-[#EFFAFD] transition-all duration-200 text-custom-pink ${
+                            message ? 'top-[-10px] text-sm' : 'top-3.5 text-base'
+                        } peer-focus:top-[-10px] peer-focus:text-sm`}
                     >
                         Message
                     </label>
                 </div>
-
-                <button className="bg-custom-dark-pink hover:bg-transparent border-2 hover:border-custom-pink text-white hover:text-custom-pink text-md font-montserrat font-semibold py-3 w-52 rounded-xl">
+                <button
+                    type="submit"
+                    className="bg-custom-dark-pink hover:bg-transparent border-2 hover:border-custom-pink text-white hover:text-custom-pink text-md font-montserrat font-semibold py-3 w-44 rounded-xl transition-all"
+                >
                     Submit
                 </button>
             </form>
@@ -85,12 +100,12 @@ const Contact = () => {
             <img
                 src={TriangleBlob}
                 alt="Triangle Blob"
-                className="absolute bottom-0 left-[-180px] w-[500px] opacity-50 z-10"
+                className="absolute -bottom-10 tablet:bottom-5 left-[-180px] tablet:left-[-200px] laptop:left-[-250px] w-[300px] tablet:w-[350px] laptop:w-[500px] opacity-50 z-10"
             />
             <img
                 src={ManyTriangleBlob}
-                alt="Inverted Pyramid Blob"
-                className="absolute top-0 right-[-320px] w-[650px] opacity-50 rotate-180 z-10"
+                alt="Inverted Blob"
+                className="hidden tablet:block absolute -top-10 tablet:-top-20 right-[-250px] tablet:right-[-320px] laptop:right-[-400px] laptop-large:right-[-350px] w-[400px] tablet:w-[500px] laptop:w-[650px] opacity-50 rotate-180 z-10"
             />
         </section>
     );
--- src/components/LandingPage/Footer.jsx
+++ src/components/LandingPage/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <footer className="bg-[#001D42] text-white py-4 text-center">
-            <p className="font-titillium font-bold text-lg">Copyright © 2023 | All Rights Reserved</p>
-            <p className="font-league font-medium text-md">Developed with DigiDev Creatives</p>
+            <p className="font-titillium font-bold tablet:text-lg mb-1">Copyright © 2025 | All Rights Reserved</p>
+            <p className="font-league font-medium text-sm tablet:text-lg">Developed with DigiDev Creatives</p>
         </footer>
     );
 };
