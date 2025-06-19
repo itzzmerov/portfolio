d8597ca46157c71870ac62243d86cb33b1153cf7
John Rovie Balingbing <balingbing.johnrovie20@gmail.com>
2025-06-19 11:04:27 +0800

    Created Footer

--- src/components/LandingPage/Footer.jsx
+++ src/components/LandingPage/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <footer className="bg-[#001D42] text-white py-4 text-center">
-            <p>Copyright © 2023 | All Rights Reserved</p>
-            <p>Developed by John Rovie Balingbing</p>
+            <p className="font-titillium font-bold text-lg">Copyright © 2023 | All Rights Reserved</p>
+            <p className="font-league font-medium text-md">Developed with DigiDev Creatives</p>
         </footer>
     );
 };
