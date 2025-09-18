40b8cfa5d773906bc90dc57478ecccb687687d2b
John Rovie Balingbing <balingbing.johnrovie20@gmail.com>
2025-09-18 18:18:34 +0800

    Added new library to fix anchor tags in navbar

--- src/components/LandingPage/Navbar.jsx
+++ src/components/LandingPage/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { FaBars, FaTimes, FaGithub } from 'react-icons/fa';
-import Logo from '../../assets/images/logo.png'
+import { Link } from 'react-scroll';
+import Logo from '../../assets/images/logo.png';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,11 +14,14 @@ const Navbar = () => {
                     <img src={Logo} alt="Logo" className="h-16 mobile:h-14" />
                     <h1 className="font-titillium font-bold text-3xl laptop:text-4xl text-custom-black">RoviDev</h1>
                 </div>
+
+                {/* Desktop Menu */}
                 <nav className="hidden tablet:flex space-x-6 laptop:space-x-10 items-center font-league font-medium text-base laptop:text-xl text-custom-darkish-blue">
-                    <a href="#home" className="hover:text-custom-pink">Home</a>
-                    <a href="#about" className="hover:text-custom-pink">About</a>
-                    <a href="#portfolio" className="hover:text-custom-pink">Portfolio</a>
-                    <a href="#contact" className="hover:text-custom-pink">Contact</a>
+                    <Link to="home" smooth={true} duration={500} className="cursor-pointer hover:text-custom-pink">Home</Link>
+                    <Link to="about" smooth={true} duration={500} className="cursor-pointer hover:text-custom-pink">About</Link>
+                    <Link to="portfolio" smooth={true} duration={500} className="cursor-pointer hover:text-custom-pink">Portfolio</Link>
+                    <Link to="contact" smooth={true} duration={500} className="cursor-pointer hover:text-custom-pink">Contact</Link>
+
                     <a
                         href="https://www.github.com/itzzmerov"
                         target="_blank"
@@ -29,21 +33,24 @@ const Navbar = () => {
                             GitHub profile
                         </span>
                     </a>
-
                 </nav>
+
+                {/* Mobile Toggle */}
                 <div className="tablet:hidden z-50">
                     <button onClick={toggleMenu}>
                         {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
                     </button>
                 </div>
             </div>
+
+            {/* Mobile Menu */}
             {isOpen && (
                 <div className="absolute top-[100%] left-0 w-full bg-[#EFFAFD] py-6 flex flex-col items-center space-y-4 text-custom-darkish-blue font-league font-semibold text-lg shadow-md tablet:hidden">
-                    <a href="#home" onClick={toggleMenu} className="hover:text-custom-pink">Home</a>
-                    <a href="#about" onClick={toggleMenu} className="hover:text-custom-pink">About</a>
-                    <a href="#portfolio" onClick={toggleMenu} className="hover:text-custom-pink">Portfolio</a>
-                    <a href="#contact" onClick={toggleMenu} className="hover:text-custom-pink">Contact</a>
-                    <a href='https://www.github.com/itzzmerov' target='_blank' rel='noreferrer'>
+                    <Link to="home" smooth={true} duration={500} onClick={toggleMenu} className="cursor-pointer hover:text-custom-pink">Home</Link>
+                    <Link to="about" smooth={true} duration={500} onClick={toggleMenu} className="cursor-pointer hover:text-custom-pink">About</Link>
+                    <Link to="portfolio" smooth={true} duration={500} onClick={toggleMenu} className="cursor-pointer hover:text-custom-pink">Portfolio</Link>
+                    <Link to="contact" smooth={true} duration={500} onClick={toggleMenu} className="cursor-pointer hover:text-custom-pink">Contact</Link>
+                    <a href="https://www.github.com/itzzmerov" target="_blank" rel="noreferrer">
                         <FaGithub className="hover:text-custom-pink cursor-pointer" size={24} />
                     </a>
                 </div>
