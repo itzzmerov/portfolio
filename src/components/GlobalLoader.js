e97675405e7b1c02a52b7c2abbcfdd39ee015f13
John Rovie R. Balingbing <balingbing.johnrovie20@gmail.com>
2025-11-03 23:13:22 +0800

    Updated Works Section

--- src/components/GlobalLoader.js
+++ src/components/GlobalLoader.js
@@ -1,48 +1,47 @@
 import { useEffect, useState } from "react";
 import { useLoader } from "../context/LoaderContext";
-import LogoImage from "../assets/images/logo.png"
+import LogoImage from "../assets/images/logo.png";
 
 const GlobalLoader = () => {
-    const text = "Loading...";
-    const { loading } = useLoader();
-    const [visible, setVisible] = useState(false);
+  const text = "Loading...";
+  const { loading } = useLoader();
+  const [visible, setVisible] = useState(false);
 
-    useEffect(() => {
-        let timer;
+  useEffect(() => {
+    let timer;
 
-        if (loading) {
-            setVisible(true);
-        } else {
-            // Keep loader visible for 3s before disappearing
-            timer = setTimeout(() => {
-                setVisible(false);
-            }, 3000);
-        }
+    if (loading) {
+      setVisible(true);
+    } else {
+      // Keep loader visible for 3s before disappearing
+      timer = setTimeout(() => {
+        setVisible(false);
+      }, 3000);
+    }
 
-        return () => clearTimeout(timer);
-    }, [loading]);
+    return () => clearTimeout(timer);
+  }, [loading]);
 
-    if (!visible) return null;
+  if (!visible) return null;
 
-    return (
-        <div className="fixed top-0 z-[1000] w-full h-screen ">
-            <div className="flex flex-col justify-center items-center h-screen bg-gray-50/90">
-                <img src={LogoImage} alt="Logo" className="" />
-                <h1 className="text-4xl font-bold flex space-x-1">
-                    {text.split("").map((letter, index) => (
-                        <span
-                            key={index}
-                            className="wave-letter"
-                            style={{ animationDelay: `${index * 0.15}s` }}
-                        >
-                            {letter}
-                        </span>
-                    ))}
-                </h1>
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="fixed top-0 z-[1000] w-full h-screen ">
+      <div className="flex flex-col justify-center items-center h-screen bg-white">
+        <img src={LogoImage} alt="Logo" className="" />
+        <h1 className="text-4xl font-bold flex space-x-1">
+          {text.split("").map((letter, index) => (
+            <span
+              key={index}
+              className="wave-letter"
+              style={{ animationDelay: `${index * 0.15}s` }}
+            >
+              {letter}
+            </span>
+          ))}
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default GlobalLoader;
--- src/components/LandingPage/Works.jsx
+++ src/components/LandingPage/Works.jsx
@@ -1,99 +1,105 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 import AOS from "aos";
 // BLOBS
-import TriangleBlob from '../../assets/images/triangle-blob.png';
-import ThreeTriangle from '../../assets/images/three-triangle-blob.png';
+import TriangleBlob from "../../assets/images/triangle-blob.png";
+import ThreeTriangle from "../../assets/images/three-triangle-blob.png";
 
 const Works = () => {
-    const [projects, setProjects] = useState([]);
-    const [visibleCount, setVisibleCount] = useState(6);
+  const [projects, setProjects] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(6);
 
-    useEffect(() => {
-        axios
-            .get('https://rovidev.pythonanywhere.com/api/projects/')
-            .then((res) => {
-                setProjects(res.data);
-                setTimeout(() => AOS.refresh(), 50);
-            })
-            .catch((err) => console.error("Error fetching projects:", err));
-    }, []);
+  useEffect(() => {
+    axios
+      .get("https://rovidev.pythonanywhere.com/api/projects/")
+      .then((res) => {
+        setProjects(res.data);
+        setTimeout(() => AOS.refresh(), 50);
+      })
+      .catch((err) => console.error("Error fetching projects:", err));
+  }, []);
 
-    const handleSeeMore = () => {
-        setVisibleCount(projects.length);
-    };
+  const handleSeeMore = () => {
+    setVisibleCount(projects.length);
+  };
 
-    return (
-        <section
-            id="portfolio"
-            className="relative overflow-hidden px-6 tablet:px-10 laptop:px-24 desktop:px-52 desktop-4k:px-80 pt-28 laptop:pt-20 pb-0 laptop:pb-10"
-        >
-            <h2 className="relative z-20 text-center text-custom-darkish-blue text-5xl tablet:text-6xl laptop:text-8xl font-titillium font-black underline mb-16" data-aos="fade-up">
-                Portfolio
-            </h2>
+  return (
+    <section
+      id="portfolio"
+      className="relative overflow-hidden px-6 tablet:px-10 laptop:px-24 desktop:px-52 desktop-4k:px-80 pt-28 laptop:pt-20 pb-0 laptop:pb-10"
+    >
+      <h2
+        className="relative z-20 text-center text-custom-darkish-blue text-5xl tablet:text-6xl laptop:text-8xl font-titillium font-black underline mb-16"
+        data-aos="fade-up"
+      >
+        Portfolio
+      </h2>
 
-            <div className="relative z-20 grid grid-cols-1 tablet:grid-cols-2 laptop-large:grid-cols-3 gap-6">
-                {projects.slice(0, visibleCount).map((item, index) => (
-                    <div
-                        key={index}
-                        data-aos="fade-up"
-                        data-aos-delay={`${index * 100}`}
-                        className="border-2 border-custom-black rounded-xl shadow-md"
-                    >
-                        <a
-                            href={item.demo_url}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex-1"
-                        >
-                            <div className="relative group w-full h-[150px] tablet:h-[200px] laptop:h-[250px] desktop:h-[270px]">
-                                <img
-                                    src={item.image}
-                                    alt={item.title}
-                                    className="w-full h-full object-cover rounded-t-xl"
-                                />
-                                <div className="absolute inset-0 bg-black/70 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-                                    <h3 className="text-white text-2xl tablet:text-3xl laptop:text-4xl font-bold font-titillium text-center px-4">
-                                        {item.title}
-                                    </h3>
-                                </div>
-                            </div>
-                        </a>
-
-                        <div className="flex w-full">
-                            <p className="w-full flex justify-center items-center text-custom-black hover:text-custom-pink font-montserrat font-semibold p-3 text-sm tablet:text-base laptop:text-xl">
-                                {item.title}
-                            </p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-
-            {/* Show "See More" if there are more items */}
-            {visibleCount < projects.length && (
-                <div className="flex justify-center mt-8">
-                    <button
-                        onClick={handleSeeMore}
-                        className="bg-custom-darkish-blue text-white text-lg px-10 py-4 rounded-xl font-semibold border-2 border-custom-darkish-blue hover:bg-transparent hover:text-custom-darkish-blue transition"
-                    >
-                        See More
-                    </button>
+      <div className="relative z-20 grid grid-cols-1 tablet:grid-cols-2 laptop-large:grid-cols-3 gap-6">
+        {projects.slice(0, visibleCount).map((item, index) => (
+          <div
+            key={index}
+            data-aos="fade-up"
+            data-aos-delay={`${index * 100}`}
+            className="border-2 border-custom-black rounded-xl shadow-md"
+          >
+            <a
+              href={item.demo_url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex-1"
+            >
+              <div className="relative group w-full h-[150px] tablet:h-[200px] laptop:h-[250px] desktop:h-[270px]">
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="w-full h-full object-cover rounded-t-xl"
+                />
+                <div className="absolute inset-0 bg-black/70 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
+                  <h3 className="text-white text-2xl tablet:text-3xl laptop:text-4xl font-bold font-titillium text-center px-4">
+                    Go to Site
+                  </h3>
                 </div>
-            )}
+              </div>
+            </a>
 
-            {/* Blobs */}
-            <img
-                src={ThreeTriangle}
-                alt="Three Triangle Blob"
-                className="absolute bottom-28 tablet:bottom-24 laptop:bottom-32 left-[-100px] tablet:left-[-150px] w-[200px] tablet:w-[300px] laptop:w-[400px] opacity-50 z-10"
-            />
-            <img
-                src={TriangleBlob}
-                alt="Triangle Blob"
-                className="absolute top-20 tablet:top-14 laptop:top-36 right-[-150px] tablet:right-[-200px] laptop:right-[-250px] w-[300px] tablet:w-[400px] laptop:w-[500px] opacity-50 rotate-180 z-10"
-            />
-        </section>
-    );
+            <div className="flex w-full flex-col">
+              <p className="w-full flex justify-center items-center text-custom-black hover:text-custom-pink font-montserrat font-semibold p-3 text-sm tablet:text-base laptop:text-xl">
+                {item.title}
+              </p>
+              <p className="w-full flex justify-center items-center text-center px-3 pb-3 text-sm tablet:text-base laptop:text-lg">
+                {item.description}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Show "See More" if there are more items */}
+      {visibleCount < projects.length && (
+        <div className="flex justify-center mt-8">
+          <button
+            onClick={handleSeeMore}
+            className="bg-custom-darkish-blue text-white text-lg px-10 py-4 rounded-xl font-semibold border-2 border-custom-darkish-blue hover:bg-transparent hover:text-custom-darkish-blue transition"
+          >
+            See More
+          </button>
+        </div>
+      )}
+
+      {/* Blobs */}
+      <img
+        src={ThreeTriangle}
+        alt="Three Triangle Blob"
+        className="absolute bottom-28 tablet:bottom-24 laptop:bottom-32 left-[-100px] tablet:left-[-150px] w-[200px] tablet:w-[300px] laptop:w-[400px] opacity-50 z-10"
+      />
+      <img
+        src={TriangleBlob}
+        alt="Triangle Blob"
+        className="absolute top-20 tablet:top-14 laptop:top-36 right-[-150px] tablet:right-[-200px] laptop:right-[-250px] w-[300px] tablet:w-[400px] laptop:w-[500px] opacity-50 rotate-180 z-10"
+      />
+    </section>
+  );
 };
 
 export default Works;
